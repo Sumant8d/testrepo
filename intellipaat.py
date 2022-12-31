56e0ff103f8d660b24d2c73090a8a7bbe5779936
Sumant Kumar <95457476+Sumant8d@users.noreply.github.com>
2022-12-30 21:58:34 -0600

    Add files via upload

---
+++ intellipaat.py
@@ -0,0 +1,20 @@
+a = [1,2,3,4,5,6,7,8,9,10]
+print(a[0]) 
+a[0]= 3333
+print(a)
+#print(a[1])
+#print(a[-1])
+#print(a[19])
+
+friends = ["A","B","C","D",45,67,89]
+print(friends[1:3])
+print(friends[-4:])
+
+num = [1,2,3,4,5,6,7,8,9,33,45,78,9000,332,11111]
+print(num)
+num.sort()
+print(num)
+num.reverse()
+print(num)
+num.insert(3,555)
+print(num)
